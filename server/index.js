c92df93a48266193d7644306ebefd5a965b59ee9
Ximanit <122241859+Ximanit@users.noreply.github.com>
2026-07-02 00:09:19 +0000

    ***

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(
 	cors({
-		origin: 'https://books-pkfal2ucn-ximanits-projects.vercel.app/',
+		origin: 'https://books-one-roan.vercel.app/',
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],

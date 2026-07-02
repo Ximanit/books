311211935a81b99c53f46f3d7e719cb8e5343359
Ximanit <122241859+Ximanit@users.noreply.github.com>
2026-07-02 00:17:57 +0000

    !

--- server/index.js
+++ server/index.js
@@ -8,14 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(
-	cors({
-		origin: 'https://books-one-roan.vercel.app',
-		credentials: true,
-		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-		allowedHeaders: ['Content-Type', 'Authorization'],
-	}),
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
